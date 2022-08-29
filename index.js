6788b2b70f8b16c528d34f77cfa2b486792802b6
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-08-29 08:02:18 -0300

    Componentes
    Se crea un componente Usuario y se renderiza a través de la función App.

--- index.js
+++ index.js
@@ -12,37 +12,39 @@ root.render(
 */
 
 const nombre = "César";
-const color = "blue";
+const color = "green";
 const sesion = true;
 const pais = undefined;
 const amigos = ["fernanda", "carlos", "camila", "fresia"];
 const frutas = ["pera", "manzana", "sandía", "durazno"];
 
-const JSX = (
-  <>
-    {sesion === true ? (
-      <div>
-        <h1 className="titulo" style={{ color: color }}>
-          Olá, Rio de Janeiro.
-        </h1>
-        <p>{nombre}, Tudo bem com você?</p>
+const Usuario = () => {
+  return (
+    <div>
+      <h1 className="titulo" style={{ color: color }}>
+        Olá, Rio de Janeiro.
+      </h1>
+      <p>{nombre}, Tudo bem com você?</p>
 
-        {pais && <p>Tu eres de {pais} </p>}
-        <ul>
-          {amigos.map((amigo, index) => {
-            return <li key={index}>{amigo} </li>;
-          })}
+      {pais && <p>Tu eres de {pais} </p>}
+      <ul>
+        {amigos.map((amigo, index) => {
+          return <li key={index}>{amigo} </li>;
+        })}
+      </ul>
+      <p>Tus frutas favoritas</p>
+      <ul>
+        {frutas.map((fruta, indice) => (
+          <li key={indice}> {fruta} </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-          {frutas.map((fruta, indice) => (
-            <li key={indice}>{fruta} </li>
-          ))}
-        </ul>
-      </div>
-    ) : (
-      <p>No has Iniciado Sesión</p>
-    )}
-  </>
-);
+const App = () => {
+  return <>{sesion === true ? <Usuario /> : <p>No has Iniciado Sesión</p>}</>;
+};
 
 /*
 const verificarSesion = (sesion) => {
@@ -53,4 +55,4 @@ const verificarSesion = (sesion) => {
   }
 };
 */
-ReactDOM.render(JSX, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
