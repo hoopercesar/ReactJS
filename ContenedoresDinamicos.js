e39b5f2528d3b4fda99841ac20442c6e8ccdc183
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-08-31 20:51:02 -0300

    contenedores dinámicos React

---
+++ ContenedoresDinamicos.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+// no spe
+// cual es
+
+const ContenedoresDinamicos = () => {
+  return (
+    <form action="">
+      <>
+        <label htmlFor="palabra">Palabra: </label>
+        <input type="text" name="palabra" id="palabra" />
+      </>
+    </form>
+  );
+};
+
+export default ContenedoresDinamicos;
