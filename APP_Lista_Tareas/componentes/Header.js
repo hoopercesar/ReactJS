bf6c067fc169712810f8b40f6ad4803bf2b1338d
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-09-08 12:27:00 -0300

    componentes Lista Tareas

---
+++ APP_Lista_Tareas/componentes/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__titulo">Lista Tareas</h1>
+      <button className="header__boton">No mostrar completadas</button>
+    </header>
+  );
+};
+
+export default Header;
