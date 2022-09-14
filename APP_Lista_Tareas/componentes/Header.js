0ef4b7aabd35c887a6cc04d5f3686fbf8ff348ef
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-09-14 07:06:54 -0300

    Add files via upload

--- APP_Lista_Tareas/componentes/Header.js
+++ APP_Lista_Tareas/componentes/Header.js
@@ -3,8 +3,11 @@ import React from "react";
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="header__titulo">Lista Tareas</h1>
-      <button className="header__boton">No mostrar completadas</button>
+      <h1 className="header__title">Lista de Tareas</h1>
+      <button className="header__boton">
+        No mostrar completadas
+        <ion-icon name="eye-off-outline"></ion-icon>
+      </button>
     </header>
   );
 };
