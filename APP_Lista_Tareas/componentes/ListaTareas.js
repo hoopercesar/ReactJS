c6ba2213c051b2d735f441c8b3052f74fe41080b
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-09-22 16:02:11 -0300

    Add files via upload

--- APP_Lista_Tareas/componentes/ListaTareas.js
+++ APP_Lista_Tareas/componentes/ListaTareas.js
@@ -1,12 +1,30 @@
 import React from "react";
 import Tarea from "./Tarea";
 
-const ListaTareas = ({ tareas }) => {
+const ListaTareas = ({ tareas, cambiarTareas }) => {
+  const toggleCompletada = (id) => {
+    console.log("ese es el id", id);
+    cambiarTareas(
+      tareas.map((tarea) => {
+        if (tarea.id === id) {
+          return { ...tarea, completado: !tarea.completado };
+        }
+        return tarea;
+      })
+    );
+  };
+
   return (
     <ul className="lista-tareas">
       {tareas.length > 0 ? (
         tareas.map((tarea) => {
-          return <Tarea key={tarea.id} tarea={tarea} />; //<li key={tarea.id}>{tarea.texto}</li>;
+          return (
+            <Tarea
+              key={tarea.id}
+              tarea={tarea}
+              toggleCompletada={toggleCompletada}
+            />
+          ); //<li key={tarea.id}>{tarea.texto}</li>;
         })
       ) : (
         <div className="lista-tareas__mensaje">"No hay tareas agregadas"</div>
