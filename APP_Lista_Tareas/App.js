7253cdf9a5bccceb597b40706cf7399ee9358c06
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-09-22 16:03:44 -0300

    Add files via upload

--- APP_Lista_Tareas/App.js
+++ APP_Lista_Tareas/App.js
@@ -1,7 +1,36 @@
+import React, { useState } from "react";
 import "./App.css";
+import Header from "./componentes/Header";
+import FormularioTareas from "./componentes/FormularioTareas";
+import ListaTareas from "./componentes/ListaTareas";
 
 const App = () => {
-  return <div>Hola, Mundo</div>;
+  const [tareas, cambiarTareas] = useState([
+    {
+      id: 1,
+      texto: "comer zapatos",
+      completado: false,
+    },
+    {
+      id: 2,
+      texto: "dormir 20 horas",
+      completado: true,
+    },
+  ]);
+
+  console.log(tareas);
+
+  return (
+    <div className="contenedor">
+      <Header />
+      <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas} />
+      <ListaTareas tareas={tareas} cambiarTareas={cambiarTareas} />
+
+      <div>
+        <ion-icon name="eye-outline"></ion-icon>
+      </div>
+    </div>
+  );
 };
 
 export default App;
