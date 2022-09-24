7812672aacc474034c086c2e1d2ddf6cd038df7c
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-09-24 07:35:45 -0300

    Add files via upload

--- APP_Lista_Tareas/componentes/Header.js
+++ APP_Lista_Tareas/componentes/Header.js
@@ -1,13 +1,24 @@
 import React from "react";
 
-const Header = () => {
+const Header = ({ mostrarCompletadas, cambiarMostrarCompletadas }) => {
+  const toggleCompletadas = () => {
+    cambiarMostrarCompletadas(!mostrarCompletadas);
+  };
+
   return (
     <header className="header">
       <h1 className="header__title">Lista de Tareas</h1>
-      <button className="header__boton">
-        No mostrar completadas
-        <ion-icon name="eye-off-outline"></ion-icon>
-      </button>
+      {mostrarCompletadas ? (
+        <button className="header__boton" onClick={() => toggleCompletadas()}>
+          No mostrar completadas
+          <ion-icon name="eye-off-outline"></ion-icon>
+        </button>
+      ) : (
+        <button className="header__boton" onClick={() => toggleCompletadas()}>
+          Mostrar completadas
+          <ion-icon name="eye-outline"></ion-icon>
+        </button>
+      )}
     </header>
   );
 };
