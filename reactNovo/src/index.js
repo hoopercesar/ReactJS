4f58b5613ed7a7e7ca35912f4b02fe7fe6e249c2
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-07-14 23:22:56 -0400

    Add files via upload

--- reactNovo/src/index.js
+++ reactNovo/src/index.js
@@ -1,34 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Hola } from "./Hola";
-import Product, { BotIcon, Navbar, TruckIcon, UsuarioInfo } from "./Product";
-import { Button } from "./Button";
+import { App } from "./App";
 
 // # al crear la función  en Hola.js se puse
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const lista = ["Tos", "Jaqueca", "Alergia"];
+
 root.render(
   <>
-    <Navbar nombre="julio" apellido="sosa" />
-    {/* <Hola title="excelente" poto="feo" />
-    <Hola title="muy bien" /> */}
-    {/* <Hola title={[1, 2, 3, 4]} /> */}
-    <Product title="red" />
-    <Navbar nombre="raquel" apellido="soto" />
-    <Navbar nombre="andrea" />
-    <UsuarioInfo
-      name="carlos"
-      apellido="Nunes"
-      amount={200}
-      adress={{ street: "holguin", number: "22" }}
-    />
-    <TruckIcon fuerza="1000N" />
-    <BotIcon />
-    <Button text="Fiebre" />
-    <Button text="Dolor Muscular" numero={55} adress={[1, 2, 3]} />
-    <Button text="Nauseas" numero={23} />
-    <Button text="Diarrea" />
-    <Button text={lista[2]} numero={5 * 2} />
+    <App />
   </>
 );
