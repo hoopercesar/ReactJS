fc1aa63a5c7e981bfb473a8625fec32dd327d8e3
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-07-14 23:23:30 -0400

    Add files via upload

--- reactNovo/src/Button.js
+++ reactNovo/src/Button.js
@@ -1,7 +1,11 @@
 import PropTypes from "prop-types";
+import "./task.css";
 
 export function Button({ text = "yes", numero = 12, adress }) {
   console.log(text, numero);
+  if (!text) {
+    console.error("El texto es requerido");
+  }
   return (
     <button>
       <ion-icon name="skull-sharp"></ion-icon> {text} {numero} {adress}{" "}
@@ -10,7 +14,27 @@ export function Button({ text = "yes", numero = 12, adress }) {
 }
 
 Button.propTypes = {
-  text: PropTypes.string,
+  text: PropTypes.string.isRequired,
   numero: PropTypes.number,
-  adress: PropTypes.object,
+  adress: PropTypes.string,
 };
+
+// esta función muestra un caminante cuando tipo es true y un avión
+// cuando tipo es false
+export function TripIcon({ tipo }) {
+  return (
+    <div>
+      {tipo ? (
+        <ion-icon name="walk"></ion-icon>
+      ) : (
+        <ion-icon name="airplane"></ion-icon>
+      )}
+    </div>
+  );
+}
+
+// se puede hacer lo mismo con los estilos
+
+export function Estilo({ ready }) {
+  return <div className={ready ? "rojo" : "verde"}>Hola</div>;
+}
