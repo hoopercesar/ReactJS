a3d48457eb2ee06f73bc4ccf8231917c93a53a68
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-08-19 23:06:26 -0400

    Add files via upload

--- reactNovo/src/index.js
+++ reactNovo/src/index.js
@@ -1,13 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-
+import { Posts } from "./Post";
 // # al crear la función  en Hola.js se puse
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const users = [
+  {
+    id: 1,
+    name: "pepe",
+    image: "https://robohash.org/pepe?set=set2",
+  },
+  {
+    id: 2,
+    name: "yoyo",
+    image: "https://robohash.org/yoyo?set=set2",
+  },
+];
+
 root.render(
   <>
-    <App />
+    <Posts />
+    {users.map((user, index) => {
+      return (
+        <div key={index}>
+          <h1>{user.name} </h1>
+          <img src={user.image} />
+        </div>
+      );
+    })}
+    {/* <App /> */}
   </>
 );
