ecda6a9f31803855494c45d4767f57a864e79d80
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-08-23 23:40:51 -0400

    Add files via upload

--- reactNovo/react_vite/App.jsx
+++ reactNovo/react_vite/App.jsx
@@ -1,19 +1,43 @@
 import TaskList from "./TaskList.jsx";
+import axios from "axios";
+import { useState } from "react";
 
 const App = () => {
+  const [data, setData] = useState();
+  const [jasonData, setJasonData] = useState();
+
+  const test = () => {
+    axios
+      .get("http://localhost:5005")
+      .then((algo) => algo.data)
+      .then((dato) => setData(dato));
+  };
+
+  const jason = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((datos) => datos.data)
+      .then((dat) => console.log(dat))
+      .then((jdt) => setJasonData(jdt));
+  };
+
   return (
     <div>
       <button
         onClick={() => {
           fetch("http://localhost:5005")
             .then((response) => response.json())
-            .then((data) => console.log(data));
-          // .catch((error) => console.log(error));
+            .then((data) => console.log(data))
+            .catch((error) => console.log(error));
         }}
       >
-        Hell
+        Fetch
       </button>
+      <p>{data}</p>
+      <button onClick={test()}>Axios</button>
       <TaskList />
+      <div>{jasonData}</div>
+      <button onClick={jason()}>Datos</button>
     </div>
   );
 };
