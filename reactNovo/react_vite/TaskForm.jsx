3ed252ba2173a949f83fd6fe250c23b11dad5970
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-08-28 23:38:25 -0400

    Add files via upload

--- reactNovo/react_vite/TaskForm.jsx
+++ reactNovo/react_vite/TaskForm.jsx
@@ -1,19 +1,35 @@
 import { useState } from "react";
+import TaskAxios from "./TaskAxios";
 
-export const TaskForm = ({ props }) => {
-  const [texto, setTexto] = useState("");
+export const TaskForm = () => {
+  const [chatUser, setChatUser] = useState("");
+  const [counter, setCounter] = useState(0);
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    setCounter(counter + 1);
+  };
+
+  const handleInputChange = (ev) => {
+    setChatUser(ev.target.value);
+  };
+
+  const handleButton = (ev) => {
+    console.log("Enviar", ev.target.value);
+  };
 
   return (
-    <form>
-      <input
-        placeholder="ingresa tu mensaje"
-        onChange={(ev) => {
-          ev.preventDefault();
-          setTexto(ev.target.value);
-        }}
-      ></input>
-      <p>{texto}</p>
-      <button>Enviar</button>
-    </form>
+    <>
+      {" "}
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="ingresa tu mensaje"
+          onChange={handleInputChange}
+        ></input>
+        <div>{chatUser}</div>
+        <button onSubmit={handleButton}>Enviar</button>
+      </form>
+      <TaskAxios userChat={chatUser} />
+    </>
   );
 };
