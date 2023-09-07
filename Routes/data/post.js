ccd929e7336ee83fa3fef08acd550a306920d899
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-09-07 18:21:54 -0300

    Add files via upload

---
+++ Routes/data/post.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    titulo: "Broncopulmonar",
+    texto:
+      "También conocido como neumólogo o especialista en enfermedades pulmonares y respiratorias es un médico especializado en el diagnóstico, tratamiento y manejo de enfermedades que afectan los pulmones y las vías respiratorias. Estos especialistas tienen un profundo conocimiento en problemas de salud pulmonar y respiratoria, desde afecciones comunes como el asma y la bronquitis hasta enfermedades pulmonares más graves como la enfermedad pulmonar obstructiva crónica (EPOC) y el cáncer de pulmón.",
+  },
+  {
+    id: 2,
+    titulo: "Dermatología",
+    texto: "Esta es la especialidad de Dermatología",
+  },
+  {
+    id: 3,
+    titulo: "Radiología",
+    texto: "Esta es la especialidad de Radiología",
+  },
+  {
+    id: 4,
+    titulo: "Neurología",
+    texto: "Esta es la especialidad de Neurología",
+  },
+];
