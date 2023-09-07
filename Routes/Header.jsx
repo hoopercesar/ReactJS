c220ec248402a0edddb665375621eb03da487127
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-09-07 18:20:56 -0300

    Add files via upload

---
+++ Routes/Header.jsx
@@ -0,0 +1,45 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <ContenedorHeader>
+      <Titulo>Clinica de Salud</Titulo>
+      <Menu>
+        <NavLink to="/">Servicios</NavLink>
+        <NavLink to="/especialidades">Especialidades</NavLink>
+        <NavLink to="/tratamientos">Tratamientos</NavLink>
+      </Menu>
+    </ContenedorHeader>
+  );
+}
+
+const ContenedorHeader = styled.header`
+  text-align: center;
+  margin-bottom: 40px;
+`;
+
+const Titulo = styled.h1`
+  margin-bottom: 10px;
+  font-size: 26px;
+  text-transform: uppercase;
+`;
+
+const Menu = styled.nav`
+  a {
+    text-decoration: none;
+    color: #165168;
+    margin: 0px 10px;
+  }
+
+  a:hover {
+    color: #19168;
+  }
+
+  a.active {
+    border-bottom: 2px solid #165168;
+    padding-bottom: 3px;
+  }
+`;
+
+export default Header;
