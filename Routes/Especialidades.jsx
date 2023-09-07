2553cd60ba685a794afdbf1f0e98096031be4f81
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-09-06 23:30:46 -0300

    Add files via upload

---
+++ Routes/Especialidades.jsx
@@ -0,0 +1,14 @@
+function Especialidades() {
+  return (
+    <div>
+      <h2>Especialidades</h2>
+      <ul>
+        <li>Broncopulmonar</li>
+        <li>Dermatología</li>
+        <li>Radiología</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Especialidades;
