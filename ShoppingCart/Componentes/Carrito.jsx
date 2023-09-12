24b919b9080443f1f8062194309e47a2f46b1aea
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-09-11 23:13:56 -0300

    Add files via upload

--- ShoppingCart/Componentes/Carrito.jsx
+++ ShoppingCart/Componentes/Carrito.jsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import { BsCart4 } from "react-icons/Bs";
+import PropTypes from "prop-types";
 
-function Carrito({ carrito }) {
+function Carrito({ carrito, eliminarUnaUnidadCarrito }) {
   return (
     <div>
       <h3>
@@ -11,10 +12,16 @@ function Carrito({ carrito }) {
       <div>
         {carrito.length > 0 ? (
           carrito.map((cart) => {
+            console.log("CARRITO-15", cart.nombre, cart.id);
             return (
               <Producto key={cart.id}>
                 <NombreProducto>{cart.nombre} </NombreProducto>
                 Cantidad: {cart.cantidad}
+                <BotonReductor
+                  onClick={() => eliminarUnaUnidadCarrito(cart.id, cart.nombre)}
+                >
+                  Menos
+                </BotonReductor>
               </Producto>
             );
           })
@@ -26,6 +33,11 @@ function Carrito({ carrito }) {
   );
 }
 
+Carrito.propTypes = {
+  carrito: PropTypes.object,
+  eliminarUnaUnidadCarrito: PropTypes.function,
+};
+
 const Producto = styled.div`
   padding: 10px;
   border-bottom: 1px solid #ebebf3;
@@ -36,6 +48,26 @@ const NombreProducto = styled.p`
   font-weight: bold;
   font-size: 16px;
   color: #000;
+`;
+
+const BotonReductor = styled.button`
+  border: none;
+  background: #ea612f;
+  color: #fff;
+  font-size: 10px;
+  font-family: "Open Sans", sans-serif;
+  text-align: center;
+  display: inline-block;
+  padding: 10px 20px;
+  cursor: pointer;
+  width: 30%;
+
+  border-radius: 10px;
+  transition: 0.3s ease all;
+
+  &:hover {
+    background: #6e2105;
+  }
 `;
 
 export default Carrito;
