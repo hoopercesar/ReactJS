b07b327c1d87bd98ecbe145733fdc214937238b0
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-09-12 23:23:55 -0300

    Add files via upload

--- ShoppingCart/Componentes/Tienda.jsx
+++ ShoppingCart/Componentes/Tienda.jsx
@@ -1,17 +1,21 @@
 import Productos from "./Productos";
 import PropTypes from "prop-types";
 
-function Tienda({ productos }) {
+function Tienda({ productos, agregarProductoCarrito }) {
   return (
     <div>
       <h2>Tienda</h2>
-      <Productos productos={productos} />
+      <Productos
+        productos={productos}
+        agregarProductoCarrito={agregarProductoCarrito}
+      />
     </div>
   );
 }
 
 Tienda.propTypes = {
-  productos: PropTypes.object,
+  productos: PropTypes.array,
+  agregarProductoCarrito: PropTypes.func,
 };
 
 export default Tienda;
