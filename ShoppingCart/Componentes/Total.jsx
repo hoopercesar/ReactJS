3779336a0f6e29dbce4ff89b56a4d90dfec3febf
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-09-14 12:48:57 -0300

    Add files via upload

--- ShoppingCart/Componentes/Total.jsx
+++ ShoppingCart/Componentes/Total.jsx
@@ -1,13 +1,15 @@
 import PropTypes from "prop-types";
 
 function Total({ carrito }) {
-  console.log(carrito);
+  let total = 0;
   if (carrito.length > 0) {
-    let total = 0;
+    console.log("Total-10", total);
     carrito.forEach((element) => {
       total += element.cantidad * element.precio;
     });
+    return total;
   }
+
   return <div>Aquí va el Total: {total} </div>;
 }
 
