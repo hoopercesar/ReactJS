f294112f5f9c3c7b5c5f6ae08aa3286e59c9b2e0
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-09-15 08:57:26 -0300

    Add files via upload

---
+++ ShoppingCart/Componentes/GeneraBoleta.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+function GeneraBoleta() {
+  return <div>GeneraBoleta</div>;
+}
+
+GeneraBoleta.propTypes = {
+  carrito: PropTypes.array,
+};
+
+export default GeneraBoleta;
