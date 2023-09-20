c3615af8ea99b208bf87e4c64701bca605eba3f2
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-09-20 13:04:18 -0300

    Add files via upload

---
+++ ContextApi/src/Controles.jsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import { useContext } from "react";
+import { ContextoTema } from "./contextos/contextoTema";
+
+function Controles() {
+  const {
+    aumentarFuente,
+    disminuirFuente,
+    alinearIzquierda,
+    alinearCentro,
+    alinearDerecha,
+  } = useContext(ContextoTema);
+
+  return (
+    <ContenedorControles>
+      <div>
+        <Boton onClick={aumentarFuente}>Aumentar Fuente</Boton>
+        <Boton onClick={disminuirFuente}>Disminuir Fuente</Boton>
+      </div>
+      <div>
+        <Boton onClick={alinearIzquierda}>Izquierda</Boton>
+        <Boton onClick={alinearCentro}>Centro</Boton>
+        <Boton onClick={alinearDerecha}>Derecha</Boton>
+      </div>
+    </ContenedorControles>
+  );
+}
+
+const ContenedorControles = styled.div`
+  margin-top: 20px;
+`;
+
+const Boton = styled.button`
+  background: #165168;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  font-size: 12px;
+  padding: 7px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  border-radius: 3px;
+
+  &:hover {
+    background: #191668;
+  }
+`;
+
+export default Controles;
