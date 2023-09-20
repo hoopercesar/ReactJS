cd083d6073474ad776acb20db295c3ab9036c02b
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-09-20 13:09:58 -0300

    Add files via upload

--- ContextApi/src/contextos/contextoTema.jsx
+++ ContextApi/src/contextos/contextoTema.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 // crea contexto que es el estado global
 const ContextoTema = React.createContext();
 
 const ProveedorTema = ({ children }) => {
+  const [tema, setTema] = useState({
+    alineado: "center",
+    fuente: 20,
+  });
+
+  const aumentarFuente = () => setTema({ ...tema, fuente: tema.fuente + 1 });
+
+  const disminuirFuente = () => setTema({ ...tema, fuente: tema.fuente - 1 });
+
+  const alinearIzquierda = () => setTema({ ...tema, alineado: "left" });
+
+  const alinearCentro = () => setTema({ ...tema, alineado: "center" });
+
+  const alinearDerecha = () => setTema({ ...tema, alineado: "right" });
+
   return (
-    <ContextoTema.Provider value={{ nombre: "Cesar" }}>
+    <ContextoTema.Provider
+      value={{
+        tema,
+        aumentarFuente,
+        disminuirFuente,
+        alinearIzquierda,
+        alinearCentro,
+        alinearDerecha,
+      }}
+    >
       {children}
     </ContextoTema.Provider>
   );
