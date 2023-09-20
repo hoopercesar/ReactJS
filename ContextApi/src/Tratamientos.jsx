932c22e2ce1725e3a1f91d759aac8544debbd857
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-09-20 13:03:02 -0300

    Add files via upload

--- ContextApi/src/Tratamientos.jsx
+++ ContextApi/src/Tratamientos.jsx
@@ -1,8 +1,11 @@
+import Controles from "./Controles";
+
 function Tratamientos() {
   return (
     <div>
       <h2>Tratamientos</h2>
       <p>Estos son los tratamientos</p>
+      <Controles />
     </div>
   );
 }
