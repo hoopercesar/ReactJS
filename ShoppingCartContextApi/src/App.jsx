e3b149f4a1461f8949f25765314317d8767a2132
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-09-20 22:23:32 -0300

    Add files via upload

--- ShoppingCartContextApi/src/App.jsx
+++ ShoppingCartContextApi/src/App.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { NavLink, BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import Inicio from "./Componentes/Inicio";
 import Blog from "./Componentes/Blog";
 import Tienda from "./Componentes/Tienda";
@@ -9,114 +8,6 @@ import Carrito from "./Componentes/Carrito";
 import GeneraBoleta from "./Componentes/GeneraBoleta";
 
 const App = () => {
-  const productos = [
-    { id: 1, nombre: "manzana", precio: 500 },
-    { id: 2, nombre: "peras", precio: 700 },
-    { id: 3, nombre: "frijoles", precio: 1500 },
-    { id: 4, nombre: "papas", precio: 5000 },
-    { id: 5, nombre: "aceite", precio: 4500 },
-    { id: 6, nombre: "bananas", precio: 900 },
-  ];
-
-  const [carrito, setCarrito] = useState([]);
-
-  const retornaPrecioProducto = (id) => {
-    const producto = productos.filter((elemento) => {
-      return elemento.id === id;
-    });
-
-    return producto;
-  };
-
-  const eliminarProducto = (id, nombre) => {
-    let algo = [];
-    if (carrito.length > 0) {
-      let clonCarrito = [...carrito];
-      algo = clonCarrito.filter((elemento) => elemento.id != id);
-    }
-    setCarrito(algo);
-  };
-
-  const eliminarUnaUnidadCarrito = (id, nombre) => {
-    const nuevoCarrito = [...carrito];
-    let [precio] = retornaPrecioProducto(id);
-
-    // primero buscamos el producto al que se desea restar cantidad
-    const [objetoCarrito] = nuevoCarrito.filter((elemento) => {
-      return elemento.id === id;
-    });
-
-    if (objetoCarrito.cantidad > 0) {
-      nuevoCarrito.forEach((elemento, index) => {
-        if (elemento.id === id) {
-          const cant = nuevoCarrito[index].cantidad;
-          nuevoCarrito[index] = {
-            id: id,
-            nombre: nombre,
-            cantidad: cant - 1,
-            precio: precio.precio,
-          };
-        }
-      });
-    }
-    setCarrito(nuevoCarrito);
-  };
-
-  const agregarProductoCarrito = (id, nombre) => {
-    let [precio] = retornaPrecioProducto(id);
-
-    // si el carrito no tiene elementos agregamos 1
-    if (carrito.length === 0) {
-      setCarrito([
-        { id: id, nombre: nombre, cantidad: 1, precio: precio.precio },
-      ]);
-    } else {
-      // primero debemos revisar si el producto está o no en el carrito
-      // si el carrito ya tiene el producto, entonces debemos actualizar la cantidad
-      // si no tiene el producto, entonces debemos agregar el producto
-      // para poder editar el arreglo carrito tenemos que clorarlo
-      const nuevoCarrito = [...carrito];
-
-      // comprobamos si nuevocarrito ya tiene el id del producto que deseamos agregar
-      const estaEnCarrito =
-        nuevoCarrito.filter((elemento) => {
-          // retorna el id si está en nuevocarrito.
-
-          return elemento.id === id;
-        }).length > 0;
-      // length>0 : true si hay elementos, false si no hay elementos en estaEnCarrito
-      // estaEnCarrito puede tomar valor true o false, sólo éso
-      // let u = [1, 2, 3]  --> u.length>0 --> true
-
-      // si ya tiene el producto, debemos actualizar la cantidad
-      if (estaEnCarrito) {
-        // para ello tenemos que buscar y obtener la posición del producto en el arreglo nuevoCarrito
-        // y en base a su posición actualizamos el valor
-        nuevoCarrito.forEach((elementoCarrito, index) => {
-          if (elementoCarrito.id === id) {
-            const cant = nuevoCarrito[index].cantidad;
-
-            nuevoCarrito[index] = {
-              id: id,
-              nombre: nombre,
-              cantidad: cant + 1,
-              precio: precio.precio,
-            };
-          }
-        });
-      } else {
-        nuevoCarrito.push({
-          id: id,
-          nombre: nombre,
-          cantidad: 1,
-          precio: precio.precio,
-        });
-      }
-      // actualizamos el contenido de carrito
-      setCarrito(nuevoCarrito);
-    }
-  };
-
   return (
     <BrowserRouter>
       {" "}
@@ -132,24 +23,12 @@ const App = () => {
             <Route path="*" element={<Error404 />} />
             <Route path="/" element={<Inicio />} />
             <Route path="/blog" element={<Blog />} />
-            <Route
-              path="/tienda"
-              element={
-                <Tienda
-                  productos={productos}
-                  agregarProductoCarrito={agregarProductoCarrito}
-                />
-              }
-            />
+            <Route path="/tienda" element={<Tienda />} />
             <Route path="boleta" element={<GeneraBoleta />} />
           </Routes>
         </main>
         <aside>
-          <Carrito
-            carrito={carrito}
-            eliminarUnaUnidadCarrito={eliminarUnaUnidadCarrito}
-            eliminarProducto={eliminarProducto}
-          />
+          <Carrito />
         </aside>
       </Contenedor>
     </BrowserRouter>
