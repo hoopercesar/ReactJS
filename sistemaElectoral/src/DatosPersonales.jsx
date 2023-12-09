4e61d413b7b506b5f7b396208e94a2bccfcb8a18
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-12-09 15:58:16 -0300

    Add files via upload

--- sistemaElectoral/src/DatosPersonales.jsx
+++ sistemaElectoral/src/DatosPersonales.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import RegionesComunas from './RegionesComunas';
-
+import VerificaRUT from './validaRut';
 
 
 function DatosPersonales() {
 
     const [nombre, setNombre] = useState('');
     const [correo, setCorreo] = useState('');
-    const [dni, setDNI] = useState('');
+    const [rut, setRUT] = useState('');
+    const [rutValido, setRutValido] = useState(false);
+    const [interactuoConRUT, setInteractuoConRUT] = useState(false);
+
+    const validaRut = (rut) => {
+        const rutNew = new VerificaRUT(rut);
+        if (rutNew.valida()) return true;
+    }
 
 
     const handleNombreChange = (event) => {
@@ -19,14 +25,22 @@ function DatosPersonales() {
         setCorreo(event.target.value);
     };
 
-    const handleDNIChange = (event) => {
-        setDNI(event.target.value);
+    const handleRUTChange = (event) => {
+        let nuevoRUT = event.target.value
+        setRUT(nuevoRUT);
+
+        // Validar el RUT y actualizar el estado
+        setRutValido(validaRut(nuevoRUT));
+
+        // Indicar que el usuario ha interactuado con el campo de RUT
+        setInteractuoConRUT(true);
+
     };
 
     const handleSubmitDatosPersonales = (event) => {
         event.preventDefault();
         // Lógica para manejar los datos personales
-        console.log('Datos personales:', { nombre, correo, dni });
+        console.log('Datos personales:', { nombre, correo, rut });
     };
 
   return (
@@ -35,12 +49,25 @@ function DatosPersonales() {
         <Input type="text" id="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} required />
 
         <Label htmlFor="correo">Correo:</Label>
-        <Input type="email" id="correo" value={correo} onChange={(e) => setCorreo(e.target.value)} required />
+        <Input 
+          type="email" 
+          id="correo" 
+          value={correo} 
+          onChange={(e) => setCorreo(e.target.value)} 
+          required 
+        />
+        
 
-        <Label htmlFor="dni">DNI:</Label>
-        <Input type="text" id="dni" value={dni} onChange={(e) => setDNI(e.target.value)} required />
-
-     
+        <Label htmlFor="dni">RUT:</Label>
+        <Input 
+          type="text" 
+          id="rut" 
+          value={rut} 
+          onChange={(e) => handleRUTChange(e)} 
+          className={interactuoConRUT && !rutValido ? 'rut-invalido' : ''}
+          required 
+        />
+        {interactuoConRUT && !rutValido && (<MensajeError>Ingrese un RUT válido</MensajeError>)}     
 
     </>
   );
@@ -58,6 +85,12 @@ const Input = styled.input`
   padding: 8px;
   margin-bottom: 10px;
   box-sizing: border-box;
+`;
+
+const MensajeError = styled.p`
+  color: red;
+  font-size: 14px;
+  margin-top: 5px;
 `;
 
 
