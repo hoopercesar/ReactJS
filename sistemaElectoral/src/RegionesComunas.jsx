2db78d692ae5f11fdf2f9abfef5bc80faf87ef9f
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-12-09 12:44:17 -0300

    Add files via upload

--- sistemaElectoral/src/RegionesComunas.jsx
+++ sistemaElectoral/src/RegionesComunas.jsx
@@ -5,12 +5,43 @@ import styled from "styled-components";
 
 // definición del tag Selección
 const Seleccion = styled.select`
-width: calc(100% - 10px); /* Ajusta el ancho para considerar el relleno */
+width: calc(70% - 10px); /* Ajusta el ancho para considerar el relleno */
 padding: 5px;
 box-sizing: border-box;
 margin-bottom: 10px;
 border-radius: 3px;
 `;    
+
+const FormularioWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+  max-width: 800px;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+`;
+
+const Label = styled.label`
+  font-weight: bold;
+  margin-bottom: 5px;
+  display: block;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 10px;
+  box-sizing: border-box;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 10px;
+  box-sizing: border-box;
+`;
 
 function RegionesComunas() {
     const [datos, setDatos] = useState([]);
@@ -69,7 +100,32 @@ function RegionesComunas() {
     }
 
     return <>
-        <Seleccion 
+
+        {/*Selección de region*/}
+        <Label htmlFor="region">Selecciona Región:</Label>
+        <Select value={regionSeleccionada} onChange={handleRegionChange} required>
+          <option value="">Selecciona una región</option>
+          {regiones.map((element, index)=>(
+                <option key={index}>{element}</option>
+            ))}
+        </Select>
+
+        {/*Selección de Comuna */}
+        <Label htmlFor="comuna">Selecciona Comuna:</Label>
+        <Select required>
+          <option value="">Selecciona una comuna</option>
+          {comunasPorRegion[regionSeleccionada] && comunasPorRegion[regionSeleccionada].map((comuna, index) => (                
+                <option key={index}>{comuna}</option>
+                ))}
+        </Select>
+
+        
+        {/*Componente candidatos con props region */}
+        <Candidatos region={regionSeleccionada} />
+
+        {/* 
+
+                <Seleccion 
             onChange={handleRegionChange} 
             value={regionSeleccionada}
         >
@@ -77,16 +133,23 @@ function RegionesComunas() {
             {regiones.map((element, index)=>(
                 <option key={index}>{element}</option>
             ))}
-        </Seleccion>
-        <Seleccion>
-            <option value="">Selecciona una Comuna</option>
-            {/* la siguiente línea contiene un condicional. si comunasPorRegion[regionSeleccionada] es true
-            entonces se ejecuta el map, si es false no se ejecuta el map */}
-            {comunasPorRegion[regionSeleccionada] && comunasPorRegion[regionSeleccionada].map((comuna, index) => (                
-            <option key={index}>{comuna}</option>
-            ))}
-        </Seleccion>
-        <Candidatos region={regionSeleccionada} />
+            </Seleccion>
+            <Seleccion>
+                <option value="">Selecciona una Comuna</option>
+                {/* la siguiente línea contiene un condicional. si comunasPorRegion[regionSeleccionada] es true
+                entonces se ejecuta el map, si es false no se ejecuta el map 
+                {comunasPorRegion[regionSeleccionada] && comunasPorRegion[regionSeleccionada].map((comuna, index) => (                
+                <option key={index}>{comuna}</option>
+                ))}
+            </Seleccion>
+
+        
+            
+            
+        
+        */}
+        
+
     </>    
 }
 
