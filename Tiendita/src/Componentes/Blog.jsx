e8d6685bcacdb47fe3faf6ff8f29cacaad17066c
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-03-01 15:14:47 -0300

    Add files via upload

--- Tiendita/src/Componentes/Blog.jsx
+++ Tiendita/src/Componentes/Blog.jsx
@@ -1,5 +1,14 @@
 function Blog() {
-    return <div>Blog</div>    
-}
-
-export default Blog;
+    return (
+      <div>
+        <h2>Deja tus comentarios</h2>
+        <p>
+          Éste puede ser un foto para dejar comentarios o ideas de clientes.
+          Puede pasar por el filtro del administrador antes de ser publicado para 
+          evitar insultos o quejas. 
+        </p>
+      </div>
+    );
+  }
+  
+  export default Blog;
