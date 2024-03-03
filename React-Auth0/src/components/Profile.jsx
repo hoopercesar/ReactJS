35c3bf53c3d056d436e0694a6a1dd2d969f203aa
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-03-02 23:43:56 -0300

    Add files via upload

---
+++ React-Auth0/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import { useAuth0 } from "@auth0/auth0-react"
+
+const Profile = () => {
+   const {user} = useAuth0();
+
+  return (
+    <div>
+        {JSON.stringify(user)}
+    </div>
+  )
+}
+
+export default Profile
