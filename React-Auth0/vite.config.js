1cfd65cd2d9159169b2ff1b1ab760b97a82a7a53
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-03-02 22:52:19 -0300

    Add files via upload

--- React-Auth0/vite.config.js
+++ React-Auth0/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// esta es la configuración que Vite permite para usar variables de ambiente (.env)
+// sin embargo, no es una opción segura porque quedan expuestas en el usuario
+// la alternaiva es usa el DOMAIN y CLIENT_ID desde el servidor.
+// de momento lo dejo así para que funcione. 
+
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd(), '');
