e0c190114728082b429752ac163ea108fb9e79f7
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-03-02 22:48:33 -0300

    Add files via upload

---
+++ React-Auth0/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+    'process.env.REACT_AUTH0_DOMAIN': JSON.stringify(env.REACT_AUTH0_DOMAIN),
+    'process.env.REACT_AUTH0_CLIENT_ID': JSON.stringify(env.REACT_AUTH0_CLIENT_ID),
+  },
+  plugins: [react()],
+  server: {
+    port: 5173,
+  },
+  }
+  
+  
+})
