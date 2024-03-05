2e53a8c884ad3fd794b0320b15e6b1be35d57c30
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-03-04 23:17:24 -0300

    Add files via upload

--- Tiendita/src/Auth0Components/Login.jsx
+++ Tiendita/src/Auth0Components/Login.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { useAuth0 } from "@auth0/auth0-react"
+import { Button } from "react-bootstrap";
 
 const Login = () => {
+
+    const {loginWithRedirect} = useAuth0();
+
   return (
-    <div>Aquí va formulario para ingresar</div>
+    <Button onClick={() => loginWithRedirect()}>Ingresar</Button>
+    
   )
 }
 
---
+++ Tiendita/src/Auth0Components/Logout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logout = () => {
+  return (
+    <div>Salir</div>
+  )
+}
+
+export default Logout
