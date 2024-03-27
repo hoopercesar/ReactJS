94c7b91061d3406ef11485cd4285d759c1e326b6
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-03-27 18:05:35 -0300

    Add files via upload

--- RutasProtegidas/src/views/LoginScreen.jsx
+++ RutasProtegidas/src/views/LoginScreen.jsx
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const LoginScreen = () => {
+
+    const navigate = useNavigate();
+    
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const user = {
+        email:"pepe@correo.cl",
+        password:"1234"
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (user.email === email && user.password === password) {
+            console.log("Sesión Iniciada");
+            localStorage.setItem('user', JSON.stringify(user.email));
+            sessionStorage.setItem('user', JSON.stringify(password));
+            navigate('/', {replace:true})
+        } else {
+            alert("Datos Incorrectos")
+        }
+    }
+
   return (
     <div>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label> Email</label>
-                <input type="email" />
+                <input 
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}                
+                />
             </div>
             <div>
                 <label >Password</label>
-                <input type="password" />
+                <input 
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} 
+                />
             </div>
             <button>Enviar</button>
             
