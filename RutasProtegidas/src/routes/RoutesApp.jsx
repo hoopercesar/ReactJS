c73dc8b52b9d0a5bb76a986ae55f2619cf8a52a6
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-03-27 18:04:58 -0300

    Add files via upload

---
+++ RutasProtegidas/src/routes/RoutesApp.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import NavigateApp from '../componentes/NavigateApp'
+import HomeScreen from '../views/HomeScreen'
+import AboutScreen from '../views/AboutScreen'
+import ErrorScreen from '../views/ErrorScreen'
+
+const RoutesApp = () => {
+  return (
+    <div>
+        <NavigateApp />
+        <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/about' element={<AboutScreen />} />
+            <Route path='*' element={<ErrorScreen />} />  
+        </Routes>
+
+    </div>
+  )
+}
+
+export default RoutesApp
