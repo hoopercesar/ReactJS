39fd1beb343a795f08758f2d614b748b5ceb4cd1
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-03-30 06:33:03 -0300

    Add files via upload

--- RutasProtegidas/src/App.jsx
+++ RutasProtegidas/src/App.jsx
@@ -1,21 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import AboutScreen from "./views/AboutScreen"
-import ErrorScreen from "./views/ErrorScreen"
-import HomeScreen from "./views/HomeScreen"
 import NavigateApp from "./componentes/NavigateApp"
 import LoginScreen from "./views/LoginScreen"
+import RoutesApp from "./routes/RoutesApp"
+import ProtectedRoutes from "./routes/ProtectedRoutes"
 
 function App() {
 
   return (
     <BrowserRouter>
-    <NavigateApp />
-    <Routes>
-      <Route path='/login' element={<LoginScreen />} />
-      <Route path='/' element={<HomeScreen />} />
-      <Route path='/about' element={<AboutScreen />} />
-      <Route path='*' element={<ErrorScreen />} />
-    </Routes>
+      
+      <Routes>
+        <Route 
+          path='/*' 
+          element={
+            <ProtectedRoutes>
+               <RoutesApp />
+            </ProtectedRoutes>            
+       } 
+        />
+        <Route path='/login' element={<LoginScreen />} />
+
+      </Routes>
     
     </BrowserRouter>
   )
