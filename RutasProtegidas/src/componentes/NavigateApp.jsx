8c508d3c77a48f10b52d0e7e4b4e7c7330f74f09
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-03-30 06:31:43 -0300

    Add files via upload

--- RutasProtegidas/src/componentes/NavigateApp.jsx
+++ RutasProtegidas/src/componentes/NavigateApp.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavigateApp = () => {
+
+    const navigate = useNavigate();
+
+  const user = JSON.parse(localStorage.getItem('user')) || null;
+  
+
+  const handleButton = () => {
+
+    localStorage.removeItem('user');
+    console.log('Usuario ha cerrado sesión');
+    navigate('/login', {replace:true});
+}
+  
+
   return (
     <>
-        <NavLink to='/'>Home</NavLink>
-        {" "}
-        <span>|</span> {" "}
-        <NavLink to='/about'>About</NavLink>
+      <NavLink to='/'>Home</NavLink>
+      {' '}
+      <span>|</span> {' '}
+      <NavLink to='/about'>About</NavLink>
+      {' '}
+      <span>|</span> {' '}
+      <button onClick={handleButton}>Logout</button>
     </>
-  )
-}
+  );
+};
 
-export default NavigateApp
+export default NavigateApp;
